5dcd9bc053fdd385844a1efb47a50b22b717fdeb
Kato Hiroki <k.hiro1818@gmail.com>
2019-11-03 15:28:21 +0900

    Add legend in graph

---
+++ src/legendwrapper.r
@@ -0,0 +1,26 @@
+require(R6)
+require(ggplot2)
+
+# See:
+# https://cran.r-project.org/web/packages/R6/R6.pdf
+# https://adv-r.hadley.nz/r6.html
+LegendWrapper <-
+  R6Class("LegendWrapper",
+    public = list(
+      initialize = function() {
+      },
+
+      # place legends inside the plot using relative coordinates between 0 and 1
+      # examples:
+      # positon = "top"
+      # positon = c(0.1, 0.9)
+      set_position = function(position) {
+        return (theme(legend.position = position))
+      },
+
+      set_edge = function() {
+        return (theme(legend.box.background = element_rect(),
+                      legend.box.margin = margin(1, 1, 1, 1)))
+      }
+    )
+  )
