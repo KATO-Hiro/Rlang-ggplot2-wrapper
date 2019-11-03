bdb74e34c6bd0e83a323ab84009643721b28bf17
Kato Hiroki <k.hiro1818@gmail.com>
2019-11-03 09:35:13 +0900

    Initial commit

---
+++ src/gridwrapper.r
@@ -0,0 +1,18 @@
+require(R6)
+require(ggplot2)
+
+# See:
+# https://cran.r-project.org/web/packages/R6/R6.pdf
+# https://adv-r.hadley.nz/r6.html
+GridWrapper <-
+  R6Class("GridWrapper",
+    public = list(
+      add_major_line = function(color) {
+        return (theme(panel.grid.major = element_line(color)))
+      },
+
+      add_minor_line = function(color) {
+        return (theme(panel.grid.minor = element_line(color)))
+      }
+    )
+  )
