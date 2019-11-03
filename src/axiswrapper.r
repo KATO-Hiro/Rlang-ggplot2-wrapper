e9b2d1d7d4354c4792ff83106ff521b18a6d5a41
Kato Hiroki <k.hiro1818@gmail.com>
2019-11-03 14:33:53 +0900

    Add exand in x and y axis

--- src/axiswrapper.r
+++ src/axiswrapper.r
@@ -23,17 +23,20 @@ AxisWrapper <-
 
       # example:
       # modify_x_axis(lower_and_upper = c(1.0, 7.0),
-      #               breaks = seq(1.0, 7.0, 0.5))
+      #               breaks = seq(1.0, 7.0, 0.5),
+      #               expand = c(0, 0))
       # seq(min_value, max_value, span)
       modify_x_axis = function(lower_and_upper, ticks) {
         return (scale_x_continuous(limits = lower_and_upper,
-                                   breaks = ticks)
+                                   breaks = ticks,
+                                   expand = c(0, 0))
         )
       },
 
       modify_y_axis = function(lower_and_upper, ticks) {
         return (scale_y_continuous(limits = lower_and_upper,
-                                   breaks = ticks)
+                                   breaks = ticks,
+                                   expand = c(0, 0))
         )
       },
 
