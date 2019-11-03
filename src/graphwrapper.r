06c9f44adc2108a4ed333997bdf7e8df6cd4dfb8
Kato Hiroki <k.hiro1818@gmail.com>
2019-11-03 18:14:11 +0900

    Add usage to graph wrapper class

--- src/graphwrapper.r
+++ src/graphwrapper.r
@@ -1,3 +1,35 @@
+# Graph wrapper for ggplot2.
+
+# Usage:
+#  scatter_plot <- ScatterPlotWrapper$new(data, aes)
+#  g <- scatter_plot$get_graph_object()
+#
+#  canvas <- scatter_plot$add_canvas()
+#  g <- g + canvas$initialize_background()
+#  g <- g + canvas$add_title(title)
+#  g <- g + canvas$add_margin()
+#
+#  axis <- scatter_plot$add_axis()
+#  g <- g + axis$add_labels(x_axis_name = "TODO: Write x axis label name.",
+#                           y_axis_name = "TODO: Write y axis label name.")
+#  g <- g + axis$set_labels_font()
+#  g <- g + axis$modify_x_axis(lower_and_upper = c(1.0, 7.0),
+#                              ticks = seq(1.0, 7.0, 0.5))
+#  g <- g + axis$modify_y_axis(lower_and_upper = c(0, 80),
+#                              ticks = seq(0, 80, 10))
+#  g <- g + axis$set_axis_font()
+#
+#  g <- g + scale_fill_manual(name = "",
+#                             values = c("green"),
+#                             labels = c("TODO: Write label name"))
+#  legend <- scatter_plot$add_legend()
+#  g <- g + legend$set_position(c(0.25, 0.9))
+#  g <- g + legend$set_font()
+#  g <- g + legend$set_edge()
+#
+#  grid <- scatter_plot$add_grid()
+#  g <- g + grid$set_default()
+
 require(R6)
 require(ggplot2)
 
